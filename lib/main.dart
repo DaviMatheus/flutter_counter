e56f7c4392fbb5dc71e0cb34423cffdb8d7a7811
DaviMatheus <matdavimat@gmail.com>
2021-07-03 16:59:02 -0300

    counter_app

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.red),
+    home: HomePage(),
+  ));
+}
+
+class HomePage  extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  void increment() {
+    counter++;
+    setState(() {
+  
+});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Counter",)),
+      body: Center(child: Text("Counter/\n$counter",
+        textAlign: TextAlign.center,
+        ),
+      ),
+      backgroundColor: Colors.white12,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {increment();},),
+    );
+  }
+}
