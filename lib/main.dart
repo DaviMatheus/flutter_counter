8d4f22e937a865471d638abb7ed429fbb27b1330
DaviMatheus <matdavimat@gmail.com>
2021-07-03 17:01:19 -0300

    changing colors

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.red),
+    theme: ThemeData(primarySwatch: Colors.purple),
     home: HomePage(),
   ));
 }
